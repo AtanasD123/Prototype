59fc4b9b7a5ddea9986afc23f83a457799bb9ade
AtanasD123 <98911730+AtanasD123@users.noreply.github.com>
2022-04-22 11:33:37 -0400

    Tab Update
    Added more plots

--- App/app.R
+++ App/app.R
@@ -14,49 +14,76 @@ library(stringr)
 library(dplyr)
 
 end_season_summary <- read_csv("seasonSummary.csv")
+player_stats <- read_csv("PlayerStats.csv")
+
 ui <- fluidPage(
   
   shinythemes::themeSelector(),
+  
+  titlePanel("Prototype"),
 
-    
-    titlePanel("Prototype"),
-    
-    sidebarPanel(
-      selectInput(inputId = "Team1",
-                  label = "Choose your team:", 
-                  choices = c("Tottenham" = "Tottenham",
-                    "Arsenal" = "Arsenal",
-                    "Manchester City" = "Manchester City",
-                    "Manchester United" = "Manchester Utd",
-                    "Chelsea" = "Chelsea",
-                    "Liverpool" = "Liverpool")),
-      
-      selectInput(inputId = "Team2",
-                  label = "Choose your second team:", 
-                  choices = c("Tottenham" = "Tottenham",
-                    "Arsenal" = "Arsenal",
-                    "Manchester City" = "Manchester City",
-                    "Manchester United" = "Manchester Utd",
-                    "Chelsea" = "Chelsea",
-                    "Liverpool" = "Liverpool")),
-      
-      selectInput(inputId = "Statistic",
-                  label = "Choose a stat to compare:",
-                  choices = c("Standing" = "Rk",
-                              "Wins" = "W",
-                              "Loses" = "L",
-                              "Draws" = "D",
-                              "Goals For" = "GF",
-                              "Goals Against" = "GA",
-                              "Goal Difference" = "GD",
-                              "Points" = "Pts",
-                              "Match Attendance" = "Attendance",
-                              "Expected Goals" = "xG"))
-    ),
-    
-    mainPanel(
-      plotOutput("Table")
-    ),
+    navbarPage(title = "Prototype",
+               tabPanel("Team Stats",
+                        sidebarPanel(
+                          selectInput(inputId = "Team1",
+                                      label = "Choose your team:", 
+                                      choices = c("Tottenham" = "Tottenham",
+                                                  "Arsenal" = "Arsenal",
+                                                  "Manchester City" = "Manchester City",
+                                                  "Manchester United" = "Manchester Utd",
+                                                  "Chelsea" = "Chelsea",
+                                                  "Liverpool" = "Liverpool")),
+                          
+                          selectInput(inputId = "Team2",
+                                      label = "Choose your second team:", 
+                                      choices = c("Tottenham" = "Tottenham",
+                                                  "Arsenal" = "Arsenal",
+                                                  "Manchester City" = "Manchester City",
+                                                  "Manchester United" = "Manchester Utd",
+                                                  "Chelsea" = "Chelsea",
+                                                  "Liverpool" = "Liverpool")),
+                          
+                          selectInput(inputId = "Statistic",
+                                      label = "Choose a stat to compare:",
+                                      choices = c("Standing" = "Rk",
+                                                  "Wins" = "W",
+                                                  "Loses" = "L",
+                                                  "Draws" = "D",
+                                                  "Goals For" = "GF",
+                                                  "Goals Against" = "GA",
+                                                  "Goal Difference" = "GD",
+                                                  "Points" = "Pts",
+                                                  "Match Attendance" = "Attendance",
+                                                  "Expected Goals" = "xG")),
+                        ),
+                        
+                        mainPanel(
+                          plotOutput("Table")
+                        ),
+               ),
+               tabPanel("Player Stats",
+                        sidebarPanel(
+                          selectInput(inputId = "Season",
+                                      label = "Choose a season",
+                                      choices = c("2020/2021" = "2021",
+                                                  "2019/2020" = "2020",
+                                                  "2018/2019" = "2019",
+                                                  "2017/2018" = "2018"
+                                      )
+                          ),
+                          selectInput(inputId = "Stat",
+                                      label = "Choose a statistic",
+                                      choices = c("Minutes Played/Goals" = "Gls",
+                                                  "Minutes Played/Assists" = "Ast"
+                                                  )
+                                      )
+                        ),
+                        mainPanel(
+                          
+                        )
+                        
+               )
+               )
 )
 
 server <- function(input, output) {
